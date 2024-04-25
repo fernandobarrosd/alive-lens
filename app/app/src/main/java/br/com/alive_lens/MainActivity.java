f0a883828a9b58184026fc935362a0420a14f066
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-04-25 19:39:33 -0300

    Init android project

---
+++ app/app/src/main/java/br/com/alive_lens/MainActivity.java
@@ -0,0 +1,12 @@
+package br.com.alive_lens;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
