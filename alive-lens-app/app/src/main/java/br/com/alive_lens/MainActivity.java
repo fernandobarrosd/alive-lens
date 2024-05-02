6ecd50fbe157d1f5790d1c30bccc8fea467f66b2
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-05-02 17:02:37 -0300

    Add MainViewModel in MainActivity

--- alive-lens-app/app/src/main/java/br/com/alive_lens/MainActivity.java
+++ alive-lens-app/app/src/main/java/br/com/alive_lens/MainActivity.java
@@ -3,35 +3,50 @@ package br.com.alive_lens;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.google.firebase.auth.FirebaseAuth;
 import br.com.alive_lens.ui.activities.AuthenticatedActivity;
 import br.com.alive_lens.ui.activities.LoginActivity;
+import br.com.alive_lens.ui.view_models.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    private MainViewModel mainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViewModel();
     }
+
+    private void initViewModel() {
+        mainViewModel = new ViewModelProvider(
+                this
+        ).get(MainViewModel.class);
+    }
+
 
     @Override
     protected void onStart() {
         super.onStart();
-        Class<?> activityClass;
+        mainViewModel.isAuthenticated().observe(this, isAuthenticated -> {
+            if (isAuthenticated) {
+                navigateToOtherActivity(AuthenticatedActivity.class);
+            }
+            else {
+                navigateToOtherActivity(LoginActivity.class);
+            }
+        });
 
-        if (isAuthenticated()) {
-            activityClass = AuthenticatedActivity.class;
-        }
-        else {
-            activityClass = LoginActivity.class;
-        }
+    }
+    public void navigateToOtherActivity(Class<?> activityClass) {
         startActivity(new Intent(this, activityClass));
         finish();
     }
-
-    private boolean isAuthenticated() {
-        FirebaseAuth firebaseAuth = FirebaseServices.getFirebaseAuth();
-        return firebaseAuth.getCurrentUser() != null;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mainViewModel.checkIsAuthenticated();
     }
 }
