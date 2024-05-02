49e04f348a387404eefa2dffc37783c2dc2ba4bc
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-05-02 16:59:13 -0300

    Create ProgressButton custom view

---
+++ alive-lens-app/app/src/main/java/br/com/alive_lens/ui/views/ProgressButton.java
@@ -0,0 +1,72 @@
+package br.com.alive_lens.ui.views;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.opengl.Visibility;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.LinearLayoutCompat;
+
+import br.com.alive_lens.R;
+import br.com.alive_lens.databinding.LayoutProgressButtonBinding;
+
+public class ProgressButton extends LinearLayoutCompat {
+    private LayoutProgressButtonBinding binding;
+    private TypedArray typedArray;
+
+    public ProgressButton(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initBinding(context);
+        initTypedArray(context, attrs);
+        setupUI();
+    }
+
+
+
+
+    private void initBinding(Context context) {
+        binding = LayoutProgressButtonBinding.inflate(
+                LayoutInflater.from(context),
+                this,
+                false
+        );
+        addView(binding.getRoot());
+    }
+
+    private void initTypedArray(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            typedArray = context.obtainStyledAttributes(attrs, R.styleable.ProgressButton);
+        }
+    }
+
+    private void setupUI() {
+        String buttonText = typedArray.getString(R.styleable.ProgressButton_buttonText);
+        typedArray.recycle();
+        removeLoading();
+
+        if (buttonText != null) {
+            binding.buttonText.setText(buttonText);
+        }
+    }
+
+    public void disable() {
+        setClickable(false);
+        setActivated(false);
+    }
+
+
+    public void showLoading() {
+        binding.buttonText.setVisibility(GONE);
+        binding.progressBar.setVisibility(VISIBLE);
+    }
+
+    public void removeLoading() {
+        binding.buttonText.setVisibility(VISIBLE);
+        binding.progressBar.setVisibility(GONE);
+    }
+
+}
