133df0621b65312938ef8cb38ebbf7d4c437ae83
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-05-16 14:00:40 -0300

    Update PostListItem ui model

--- alive-lens-app/app/src/main/java/br/com/alive_lens/ui/models/PostListItem.java
+++ alive-lens-app/app/src/main/java/br/com/alive_lens/ui/models/PostListItem.java
@@ -1,11 +1,16 @@
 package br.com.alive_lens.ui.models;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(setterPrefix = "set")
 public class PostListItem {
     private String id;
     private String content;
+    private String mediaURL;
+    private Integer likes;
 }
