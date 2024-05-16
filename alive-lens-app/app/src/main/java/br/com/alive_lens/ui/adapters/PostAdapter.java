e8dc5487886d772cd8c06e1e7dc67b760311cd8d
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-05-16 12:33:00 -0300

    Update PostAdapter recyclerview

--- alive-lens-app/app/src/main/java/br/com/alive_lens/ui/adapters/PostAdapter.java
+++ alive-lens-app/app/src/main/java/br/com/alive_lens/ui/adapters/PostAdapter.java
@@ -1,6 +1,7 @@
 package br.com.alive_lens.ui.adapters;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,31 +9,59 @@ import java.util.List;
 import br.com.alive_lens.databinding.PostItemBinding;
 import br.com.alive_lens.ui.models.PostListItem;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import br.com.alive_lens.R;
 
 @RequiredArgsConstructor
-public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
+public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final List<PostListItem> posts;
+
+    private boolean isSkeleton;
 
     @NonNull
     @Override
-    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        PostItemBinding binding = PostItemBinding.inflate(
-                LayoutInflater.from(parent.getContext()),
-                parent,
-                false
-        );
-        return new PostViewHolder(binding);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == 0) {
+            PostItemBinding binding = PostItemBinding.inflate(
+                    LayoutInflater.from(parent.getContext()),
+                    parent,
+                    false
+            );
+            return new PostViewHolder(binding);
+        }
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.post_item_skeleton, parent, false);
+
+        return new PostViewHolderSkeleton(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PostViewHolder postViewHolder, int position) {
-        PostListItem post = posts.get(position);
-        postViewHolder.bind(post);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        if (viewHolder instanceof PostViewHolder) {
+            PostListItem post = posts.get(0);
+            PostViewHolder postViewHolder = (PostViewHolder) viewHolder;
+            postViewHolder.bind(post);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isSkeleton) {
+            return 1;
+        }
+        return 0;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (isSkeleton) {
+            return 5;
+        }
+        return posts.size();
+    }
+
+    public void setSkeleton(boolean skeleton) {
+        isSkeleton = skeleton;
     }
 
     public static class PostViewHolder extends RecyclerView.ViewHolder {
@@ -43,6 +72,12 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
         public void bind(PostListItem post) {
 
+        }
+    }
+
+    public static class PostViewHolderSkeleton extends RecyclerView.ViewHolder {
+        public PostViewHolderSkeleton(@NonNull View itemView) {
+            super(itemView);
         }
     }
 }
