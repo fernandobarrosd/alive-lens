f70b48f86fbc6d3e1bcc85246d7f01e135ac56cd
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-05-16 18:27:31 -0300

    Update PostAdapter recyclerview

--- alive-lens-app/app/src/main/java/br/com/alive_lens/ui/adapters/PostAdapter.java
+++ alive-lens-app/app/src/main/java/br/com/alive_lens/ui/adapters/PostAdapter.java
@@ -3,11 +3,17 @@ package br.com.alive_lens.ui.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 import br.com.alive_lens.databinding.PostItemBinding;
 import br.com.alive_lens.ui.models.PostListItem;
+import br.com.alive_lens.ui.models.PostOwner;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import br.com.alive_lens.R;
@@ -68,10 +74,37 @@ public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         private PostItemBinding binding;
         public PostViewHolder(@NonNull PostItemBinding binding) {
             super(binding.getRoot());
+            this.binding = binding;
         }
 
         public void bind(PostListItem post) {
+            PostOwner postOwner = post.getPostOwner();
+            String postOwnerUsername = postOwner.getUsername();
+            String postOwnerPhoto = postOwner.getPhoto();
+            String likeText = post.getLikes().toString() + " ";
 
+            binding.postContent.setText(post.getContent());
+            setupImageViewImageURL(binding.postOwnerProfile, postOwnerPhoto);
+            binding.postOwnerUsername.setText(postOwnerUsername);
+
+            if (post.getLikes() == 0) {
+                binding.likesNumberText.setVisibility(View.GONE);
+            }
+            else {
+                if (post.getLikes() == 1) {
+                    likeText += "curtiu isso";
+                }
+                else {
+                    likeText += "curtiram isso";
+                }
+                binding.likesNumberText.setText(likeText);
+            }
+        }
+
+        private void setupImageViewImageURL(ImageView imageView, String imageURL) {
+            Glide.with(binding.getRoot())
+                    .load(imageURL)
+                    .into(imageView);
         }
     }
 
