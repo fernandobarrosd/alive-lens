09b696d0c434b7050c7f61e131b195492218847e
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-05-29 19:35:50 -0300

    Create AuthActivity with navigation

--- alive-lens-app/app/src/main/java/br/com/alive_lens/ui/auth/AuthActivity.java
+++ alive-lens-app/app/src/main/java/br/com/alive_lens/ui/auth/AuthActivity.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import br.com.alive_lens.databinding.ActivityAuthBinding;
+import lombok.Getter;
+import lombok.Setter;
 
 public class AuthActivity extends AppCompatActivity {
     private ActivityAuthBinding binding;
+    @Getter @Setter
+    private String username, email, password, photoURL;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
